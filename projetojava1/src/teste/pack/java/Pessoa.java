b9cdcb746830194c319353e87135e61940fd6e08
Anderson Leoni <Anderson Leoni@Anderson>
2024-05-22 21:19:30 -0300

    class nova

--- projetojava1/src/teste/pack/java/Pessoa.java
+++ projetojava1/src/teste/pack/java/Pessoa.java
@@ -4,6 +4,8 @@ public class Pessoa {
 	private String nome;
 	
 	private String sobrenome;
+	
+	private int idade;
 
 	public String getNome() {
 		return nome;
@@ -19,5 +21,13 @@ public class Pessoa {
 
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 }
