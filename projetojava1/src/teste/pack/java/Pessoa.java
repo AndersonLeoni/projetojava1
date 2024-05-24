2b462e82017e48443f83eff5a39933e68d555776
Anderson Leoni <Anderson Leoni@Anderson>
2024-05-23 23:20:38 -0300

    nova classe adicionada, data de nascimento

--- projetojava1/src/teste/pack/java/Pessoa.java
+++ projetojava1/src/teste/pack/java/Pessoa.java
@@ -6,10 +6,16 @@ public class Pessoa {
 	private String sobrenome;
 	
 	private int idade;
+	
+	private int tel; 
+	
+	private Date dataNascimento;
 
 	public String getNome() {
 		return nome;
 	}
+	
+	
 
 	public void setNome(String nome) {
 		this.nome = nome;
@@ -30,4 +36,29 @@ public class Pessoa {
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+
+
+
+	public int getTel() {
+		return tel;
+	}
+
+
+
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+
+
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
 }
